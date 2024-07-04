0ae7ea309beeb717ca5424027826bb1a79aaa9df
= <tsnurali@mail.ru>
2024-07-04 17:46:31 +0500

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -48,7 +49,11 @@ if __name__ == '__main__':
     train_df, test_df = a.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_df, test_df)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_df, test_df)
+    
+    modelTrainer = ModelTrainer()
+    res = modelTrainer.initiate_model_trainer(train_arr, test_arr)
+    print(res)
     
     
 
